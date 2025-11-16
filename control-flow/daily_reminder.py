30c84113c0e92400035b1ea60797baa3ff36084a
Gleta Kijogi <gkkijogi@gmail.com>
2025-11-16 14:25:15 +0300

    adjusted if statements

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -3,10 +3,20 @@ TimeBound = input("Is it time-bound? (yes/no):" )
 Priority = input("Priority (high/medium/low):" )
 match Priority:
     case "high":
-        print("'", Task, "' is a high priority task that requires immediate attention today!")
+        print("'", Task, "' is a high priority task")
+        if TimeBound == "yes":
+            print("that requires immediate attention today!")
+        else:
+            print("Consider completing it when you have free time.")
     case "medium":
-        print("'", Task, "' is a medium priority task. Consider completing it after you're done with the high priority tasks for the day!")
+        print("'", Task, "' is a medium priority task.")
+        if TimeBound == "yes":
+            print("that requires immediate attention today!")
+        else:
+            print("Consider completing it when you have free time.")
     case "low":
         print("'", Task, "' is a low priority task. Consider completing it when you have free time.")
-if TimeBound == "yes":
-    print("This is also a time-bound task. Complete it as a soon as possible!")
+        if TimeBound == "yes":
+            print("that requires immediate attention today!")
+        else:
+            print("Consider completing it when you have free time.")
