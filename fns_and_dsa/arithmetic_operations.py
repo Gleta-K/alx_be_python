4242e5d3908199eba5220cf2fc40408f1973e9b8
Gleta Kijogi <gkkijogi@gmail.com>
2025-11-23 22:42:35 +0300

    adjusted perform_operation function

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,25 +1,17 @@
 def perform_operation(num1: float, num2: float, operation: str):
     if operation == "add":
-        result = num1 + num2
+        return num1 + num2
+
     elif operation == "subtract":
-        result = num1 - num2
+        return num1 - num2
+
     elif operation == "multiply":
-        result = num1 * num2
+        return num1 * num2
+
     elif operation == "divide":
         if num2 == 0:
-            result = print(f"invalid operation")
-            return
-        else:
-            result = num1/num2
-        return
-    
-    from arithmetic_operations import perform_operation
-    
-def main():
-    print("Arithmetic Operations")
-    num1 = float(input("Enter the first number: "))
-    num2 = float(input("Enter the second number: "))
-    operation = input("Enter the operation (add, subtract, multiply, divide): ").strip().lower()
+            return "invalid operation"
+        return num1 / num2
 
-    result = perform_operation(num1, num2, operation)
-    print(f"Result: {result}")
+    else:
+        return "unknown operation"
