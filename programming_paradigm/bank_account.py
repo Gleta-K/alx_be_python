f8cbe83d639c5c979cfc03e2298b9ac0420632a8
Gleta Kijogi <gkkijogi@gmail.com>
2025-11-30 23:01:16 +0300

    add programming_paradigm

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,18 @@
+class BankAccount:
+    def __init__(self, initial_balance=0):
+        self.account_balance = initial_balance   # encapsulated attribute
+
+    def deposit(self, amount):
+        """Add money to the account balance."""
+        self.account_balance += amount
+
+    def withdraw(self, amount):
+        """Deduct amount only if sufficient funds exist."""
+        if amount <= self.account_balance:
+            self.account_balance -= amount
+            return True
+        return False
+
+    def display_balance(self):
+        """Print the current balance."""
+        print(f"Current Balance: ${self.account_balance}")
